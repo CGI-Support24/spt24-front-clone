7104f5211a4043ada09d7cf3cf45d4b34eed5e11
고소명 <flafla.ovo@gmail.com>
2022-12-26 11:21:12 +0900

    dhldksgehla

--- Support24/src/main.js
+++ Support24/src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import axios from 'axios'
+import Vue from 'vue'
 import CoreuiVue from '@coreui/vue'
 import CIcon from '@coreui/icons-vue'
 import { iconsSet as icons } from '@/assets/icons'
@@ -14,3 +15,4 @@ app.use(CoreuiVue)
 app.provide('icons', icons)
 app.component('CIcon', CIcon)
 app.mount('#app')
+Vue.prototype.$axios = axios
---
+++ Support24/src/views/adminpage/adminpage.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="main">
+    <CRow>
+      <CCol :md="12">
+        <CCard class="mb-4">
+          <CCardBody>
+            <CRow>
+              <CCol :md="4">
+                <h3>관리자 페이지</h3>
+                <br />
+              </CCol>
+              <CRow>
+                <h4>오늘 추가된 지원사업!</h4>
+                <br />
+                <CCol :md="3">
+                  <h3>공고제목 and 이미지</h3>
+                  <p>
+                    공고 내용 여기는 지원사업 내용을 <br />쓰는 자리 입니다.
+                  </p>
+                </CCol>
+                <CCol :md="3">
+                  <h3>공고제목 and 이미지</h3>
+                  <p>
+                    공고 내용 여기는 지원사업 내용을 <br />쓰는 자리 입니다.
+                  </p>
+                </CCol>
+                <CCol :md="3">
+                  <h3>공고제목 and 이미지</h3>
+                  <p>
+                    공고 내용 여기는 지원사업 내용을 <br />쓰는 자리 입니다.
+                  </p>
+                </CCol>
+                <CCol :md="3">
+                  <h3>공고제목 and 이미지</h3>
+                  <p>
+                    공고 내용 여기는 지원사업 내용을 <br />쓰는 자리 입니다.
+                  </p>
+                </CCol>
+                <br />
+                <br />
+                <br />
+                <h4>DB키워드 오류 수정</h4>
+                <p>이곳에는 DB에 들어가 있는 키워드들이 나열됩니다.</p>
+              </CRow>
+            </CRow>
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  </div>
+</template>
+
+<!-- 백단에서 넣어야 하는 데이터들은 여기아래에-->
+<script>
+const HOST = "http://221.156.243.172:3003";
+export default {
+  components: {},
+  setup() {
+    return {}
+  },
+ getData(){
+  this.$axios
+  .get(HOST + "/adminpage/getAll")
+  .then((res)=>{
+    console.log(res.staus);
+    console.los(res.data);
+  })
+  .catch((error)=>{
+    console.log(error);
+  })
+  .finally(()=>{
+    console.log("종료")
+  })
+ } 
+  
+
+
+}
+
+</script>
+<style></style>
